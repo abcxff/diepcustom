5ca7019fe8cfe1aeb89229ab0cf86443905e43ac
Nulled <63370961+Nul-led@users.noreply.github.com>
2025-07-03 14:55:22 +0200

    Update AutoTurret.ts

--- src/Entity/Tank/AutoTurret.ts
+++ src/Entity/Tank/AutoTurret.ts
@@ -123,7 +123,7 @@ export default class AutoTurret extends ObjectEntity {
      * Spreads onKill to owner
      */
     public onKill(killedEntity: LivingEntity) {
-        this.owner?.onKill?.(killedEntity);
+        (this.owner as LivingEntity)?.onKill?.(killedEntity);
     }
 
     public tick(tick: number) {
